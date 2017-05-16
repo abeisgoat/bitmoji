1470ba9cc7d926267a92b57d50181bfe8c212a34
Abraham Haskins <abeisgreat@abeisgreat.com>
2017-05-15 23:04:11 -0700

    Remove dist

--- dist/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-require("@types/node");
-function Perform(text) {
-    return Promise.resolve(`A {text} promise!`);
-}
-exports.Perform = Perform;
-//# sourceMappingURL=index.js.map
